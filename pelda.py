547287d54a651052e896125d5428a07c5e26d0a7
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2025-12-10 08:17:30 +0100

    Update pelda.py

--- pelda.py
+++ pelda.py
@@ -2,5 +2,33 @@ ures_szotar = {}
 
 szemely = {
     "nev": "Kovács János",
-    "kor": 30,
-} 
+    "kor": 25,
+    "telefon": "SAMSUNG",
+    "varos": "Budapest",
+    } 
+
+print(szemely["nev"])
+print(szemely["kor"])
+
+
+print(szemely.get("telefon", "Nincs a szótárban"))
+
+szemely["kor"] = 26
+print(szemely["kor"])
+
+szemely["neme"] = "Férfi"
+print(szemely.get("neme"))
+
+
+print(szemely["varos"])
+del szemely["varos"]
+print(szemely.get("varos", "Nincs a szótárban"))
+
+if "nev" in szemely:
+    print("A keresett megtalálható a szótárban.")
+
+if"bankszamla" not in szemely:
+    print("A keresett kulcs nem található.")
+
+for kulcs, ertek in szemely.items():
+    print(f"{kulcs}, {ertek}")
